4d3bd39569346dfe8930ed84eba60d9c6baf6603
André Santos <andrefs@andrefs.com>
2017-09-29 18:16:28 +0100

    Fardalheira na printqueue

--- printqueue.js
+++ printqueue.js
@@ -1,26 +1,93 @@
 const amqp = require('amqplib/callback_api');
 
-const printAPI = require('./lib/converter/');
+var async      = require('async');
+var paths      = require('./lib/config').PATHS;
+var printer    = require('./lib/printer').printer;
+var fs         = require('fs.extra');
+var glob       = require('glob');
+var moment     = require('moment');
 
 amqp.connect('amqp://127.0.0.1', function(err, conn) {
-    console.log(conn);
   conn.createChannel(function(err, ch) {
     var q = 'queue';
-
     ch.assertQueue(q, {durable: false});
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+
     ch.consume(q, function(msg) {
-      console.log(" [x] Received %s", msg.content.toString());
+      // var data = JSON.parse
+      var data = {
+        url: 'https://subvisual.co/images/team/roberto-machado-1696a4b6.jpg',
+        contrast: 100,
+        brightness: 50
+      };
 
-
-        // msg.content is of the form "file=file.png contrast=1 brightness=1"
-        const keyValues = msg.content.toString().split(' ');
-        const file = keyValues[0].split('=')[1];
-        const contrast = keyValues[1].split('=')[1];
-        const brightness = keyValues[2].split('=')[1];
-
-        printAPI.generateImages(file, contrast, brightness);
+      printFull(data.url, data.contrast, data.brightness, data.opts, console.log);
 
     }, {noAck: true});
   });
 });
+
+function printFull(tmpPath, contrast, brightness, opts, callback){
+  var z1 = ((+contrast-1)/(2*+brightness*+contrast));
+  var z2 = ((+contrast+1)/(2*+brightness*+contrast));
+  var z2   = +req.POSTargs['z2'];
+  var imParams = [
+      '-resize'    , '384x2000',
+      '-level'     , (z1*100)+'%,'+(z2*100)+'%',
+      '-colorspace', 'gray'
+  ];
+
+  var timestamp = new Date().getTime();
+  var tmpPath      = file.path;
+  var originalPath = paths.originals+'/original_'+timestamp;
+  var finalPaths   = paths.finals+'/final_'+timestamp;
+
+  async.series([
+    // Move file to originals folder
+    function(next){
+        fs.move(tmpPath, originalPath, next);
+    },
+    // Convert and keep result in finals folder
+    function(next){
+        convert(originalPath, finalPaths+'-%d.png', imParams, next);
+    },
+    // print pixels camp logo
+    function(next){
+      printer.printImages([paths.system+'/pixels.png'], opts, next);
+    },
+    // print image
+    function(next){
+      glob(finalPaths+'*', {}, function(err, files){
+        if(err){ return next(err); }
+        printer.printImages(files, opts, next);
+      });
+    },
+    // print date
+    function(next){
+      var date = new Date();
+      var today = date.toISOString().substr(0,10);
+      var hour  = date.toISOString().substr(11,4);
+
+      printer.printLines([
+        '',
+        '   Printed on '+moment().format('YYYY.MM.DD HH:mm'),
+        //'',
+      ], opts, next);
+    },
+    // print footer
+    function(next){
+      printer.printImages([paths.system+'/footer.png'], opts, next);
+    },
+    // print url
+    function(next){
+      printer.printLines([
+        '',
+        '     pixels.camp/projects/16',
+        '',
+        '',
+        '',
+        ''
+      ], opts, next);
+    },
+  ], callback);
+}
